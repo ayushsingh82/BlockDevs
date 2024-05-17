12c6aaa8c80a7b851deb6d1e0379793855162125
Samarth <sam@Samarths-MacBook-Air.local>
2024-05-17 21:40:27 +0530

    Navbar done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,13 +16,16 @@ const config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        "custom-pink": "#FF4ECD",
+      },
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
       },
