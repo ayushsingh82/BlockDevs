3813d00f6f4c7f22fc8496672678f3403c9d9cab
ayushsingh82 <ayushsinghmi711@gmail.com>
2024-05-19 14:51:46 +0530

    cards

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['th.bing.com', 'www.creativefabrica.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
--- src/components/Blockchains/Blockchains.tsx
+++ src/components/Blockchains/Blockchains.tsx
@@ -2,10 +2,14 @@
 import React from 'react'
 import { motion } from "framer-motion";
 import { LampContainer } from "../ui/lamp";
+import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
+import Link from "next/link";
+import Image from "next/image";
+import { EvervaultCard, Icon } from "../ui/evervault-card";
 
 function Blockchains() {
   return (
-    <div>
+    <div className=''>
         <LampContainer>
       <motion.h1
         initial={{ opacity: 0.5, y: 100 }}
@@ -20,6 +24,71 @@ function Blockchains() {
         Explore Blockchains <br /> & Protocols
       </motion.h1>
     </LampContainer>
+
+    <div className='bg-black'>
+      </div>
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white"
+        >
+          Make things float in air
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          Hover over this card to unleash the power of CSS perspective
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <Image
+            src="https://th.bing.com/th/id/OIP.FgGFG3YWH-aFkG1a2VTMOQAAAA?rs=1&pid=ImgDetMain"
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-between items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as={Link}
+            href="https://twitter.com/mannupaaji"
+            target="__blank"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+          >
+            Try now →
+          </CardItem>
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+          >
+            Sign up
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+
+    <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[30rem] ">
+      <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
+      <Icon className="absolute h-6 w-6 -bottom-3 -right-3 dark:text-white text-black" />
+ 
+      <EvervaultCard text="Chainlink" />
+ 
+      <h2 className="dark:text-white text-black mt-4 text-sm font-light">
+        Hover over this card to reveal an awesome effect. Running out of copy
+        here.
+      </h2>
+      <p className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5">
+        Watch me hover
+      </p>
+    </div>
+
     </div>
   )
 }
